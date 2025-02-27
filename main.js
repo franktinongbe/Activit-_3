8b1eadff9b983795f1790c3bf958c0f4b7d52049
unknown <franktinongbe86@gmail.com>
2025-02-27 23:12:42 +0100

    [ADD]:.env copy files added Devise not difined

---
+++ main.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+  const countriesCont= document.getElementById('countries');
+  fetch('https://restcountries.com/v3.1/all').then(reponse=>reponse.json()).then(countries=>{
+    const countriesList=countries.slice(0,20);
+
+    let call='';
+    countriesList.forEach(country => {call+=`<div class="country"> <h3>${country.name.common}</h3>
+      <img src="${country.flags.png}" alt="drapeau"> 
+      <p><strong> Capital:  </strong>${country.capital ? country.capital[0]: ''}</p> 
+       <h3><strong>Devise:</strong>${country.currencies.name} </h3> </div>`;  
+    });
+    countriesCont.innerHTML=call;
+    
+  })
+  .catch(error=> {
+    console.error('erreur:', error);
+   });
+  });
+ 
